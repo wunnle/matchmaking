df89fb63a951db00d4fcfff2de03f8f8f161d33f
Sinan Aksay <me@wunnle.com>
2020-11-16 00:04:06 +0300

    better google token checks

--- pages/api/createGame.js
+++ pages/api/createGame.js
@@ -8,7 +8,11 @@ async function createGame(req, res) {
       const { settings } = req.body
       const { map, maxPlayers, gameCode, playerName } = settings
 
+      // verify google id_token & get userId
+
       const client = new OAuth2Client(process.env.NEXT_PUBLIC_GOOGLE_ID)
+
+      let payload
 
       try {
         const ticket = await client.verifyIdToken({
@@ -16,41 +20,41 @@ async function createGame(req, res) {
           audience: process.env.NEXT_PUBLIC_GOOGLE_ID
         })
 
-        const payload = ticket.getPayload()
-
-        const userId = payload.sub
-
-        const { db } = await connectToDatabase()
-
-        const collection = db.collection('games')
-
-        // check if host has a game already
-
-        const searchQuery = { hostId: userId }
-        const searchOptions = {
-          sort: { rating: -1 },
-          projection: { _id: 1 }
-        }
-
-        const existingGame = await collection.findOne(searchQuery, searchOptions)
-
-        if (existingGame) {
-          res.status(200).json({ message: 'already exists', gameId: existingGame._id })
-        } else {
-          const doc = {
-            createdAt: new Date(),
-            settings: { map, maxPlayers, gameCode, host: playerName },
-            hostId: userId,
-            players: [{ name: playerName, userId }],
-            playerCount: 1,
-            isFull: false
-          }
-          const result = await collection.insertOne(doc)
-          res.status(200).json({ message: 'created', gameId: result.insertedId })
-        }
+        payload = ticket.getPayload()
       } catch (error) {
         res.status(403).json({ message: 'Invalid Google token' })
         return
+      }
+
+      const userId = payload.sub
+
+      const { db } = await connectToDatabase()
+
+      const collection = db.collection('games')
+
+      // check if host has a game already
+
+      const searchQuery = { hostId: userId }
+      const searchOptions = {
+        sort: { rating: -1 },
+        projection: { _id: 1 }
+      }
+
+      const existingGame = await collection.findOne(searchQuery, searchOptions)
+
+      if (existingGame) {
+        res.status(200).json({ message: 'already exists', gameId: existingGame._id })
+      } else {
+        const doc = {
+          createdAt: new Date(),
+          settings: { map, maxPlayers, gameCode, host: playerName },
+          hostId: userId,
+          players: [{ name: playerName, userId }],
+          playerCount: 1,
+          isFull: false
+        }
+        const result = await collection.insertOne(doc)
+        res.status(200).json({ message: 'created', gameId: result.insertedId })
       }
     } else {
       res.status(400).end('bad request')
--- pages/api/findAndJoinGame.js
+++ pages/api/findAndJoinGame.js
@@ -8,80 +8,87 @@ async function findGame(req, res) {
       const { settings } = req.body
       const { map, playerName } = settings
 
+      // verify google id_token & get userId
+
+      const client = new OAuth2Client(process.env.NEXT_PUBLIC_GOOGLE_ID)
+
+      let payload
+
       try {
-        const client = new OAuth2Client(process.env.NEXT_PUBLIC_GOOGLE_ID)
         const ticket = await client.verifyIdToken({
           idToken: req.body.id_token,
           audience: process.env.NEXT_PUBLIC_GOOGLE_ID
         })
 
-        const payload = ticket.getPayload()
-
-        const userId = payload.sub
-
-        // check if player has a game already as host
-
-        const { db } = await connectToDatabase()
-
-        const collection = db.collection('games')
-
-        const searchQuery = { hostId: userId }
-        const searchOptions = {
-          sort: { players: 1 },
-          projection: { _id: 1 }
-        }
-
-        const existingGame = await collection.findOne(searchQuery, searchOptions)
-
-        if (existingGame) {
-          res.status(200).json({ message: 'already exists', gameId: existingGame._id })
-          return
-        }
-
-        const bestGameQuery = { isFull: false, 'settings.map': map }
-
-        const bestGameOptions = {
-          sort: { playerCount: -1 }
-        }
-
-        const bestGame = await collection.findOne(bestGameQuery, bestGameOptions)
-
-        if (!bestGame) {
-          res.status(200).json({ message: 'no games available' })
-        } else {
-          if (bestGame.players.some(p => p.userId === userId)) {
-            res.status(200).json({ message: 'already in game', gameId: bestGame._id })
-            return
-          }
-
-          const update = {
-            $set: {
-              playerCount: bestGame.playerCount + 1,
-              isFull: !(bestGame.playerCount < bestGame.settings.maxPlayers),
-              players: [
-                ...bestGame.players,
-                {
-                  name: playerName,
-                  userId
-                }
-              ]
-            }
-          }
-
-          const updatedGame = await collection.findOneAndUpdate(
-            { _id: bestGame._id },
-            update,
-            { returnNewDocument: true }
-          )
-
-          if (updatedGame) {
-            res.status(200).json({ message: 'updated game', gameId: bestGame._id })
-          }
-        }
+        payload = ticket.getPayload()
       } catch (error) {
         res.status(403).json({ message: 'Invalid Google token' })
         return
       }
+
+      const userId = payload.sub
+
+      // check if player has a game already as host
+
+      const { db } = await connectToDatabase()
+
+      const collection = db.collection('games')
+
+      const searchQuery = { hostId: userId }
+      const searchOptions = {
+        sort: { players: 1 },
+        projection: { _id: 1 }
+      }
+
+      const existingGame = await collection.findOne(searchQuery, searchOptions)
+
+      if (existingGame) {
+        res.status(200).json({ message: 'already exists', gameId: existingGame._id })
+        return
+      }
+
+      const bestGameQuery = { isFull: false, 'settings.map': map }
+
+      const bestGameOptions = {
+        sort: { playerCount: -1 }
+      }
+
+      const bestGame = await collection.findOne(bestGameQuery, bestGameOptions)
+
+      if (!bestGame) {
+        res.status(200).json({ message: 'no games available' })
+      } else {
+        if (bestGame.players.some(p => p.userId === userId)) {
+          res.status(200).json({ message: 'already in game', gameId: bestGame._id })
+          return
+        }
+
+        const update = {
+          $set: {
+            playerCount: bestGame.playerCount + 1,
+            isFull: !(bestGame.playerCount < bestGame.settings.maxPlayers),
+            players: [
+              ...bestGame.players,
+              {
+                name: playerName,
+                userId
+              }
+            ]
+          }
+        }
+
+        const updatedGame = await collection.findOneAndUpdate(
+          { _id: bestGame._id },
+          update,
+          { returnNewDocument: true }
+        )
+
+        if (updatedGame) {
+          res.status(200).json({ message: 'updated game', gameId: bestGame._id })
+        }
+      }
+    } else {
+      res.status(400).end('bad request')
     }
   } else {
     res.status(405).end('method not allowed')
